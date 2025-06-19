fee37dfecc25fb4b0642ed46870b138f343d0832
Kaliyappan1 <yappank17@gmail.com>
2025-06-19 21:24:44 +0530

    updated app meta

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="Splunk Lab – Soft Mania" />
+        <meta property="og:description" content="Quickly deploy cost-effective Splunk labs..." />
+        <meta property="og:image" content="https://splunklab.softmania.in/og-cover.png" />
+        <meta property="og:url" content="https://splunklab.softmania.in" />
+        <meta property="og:type" content="website" />
+
         <link rel="canonical" href="https://splunklab.softmania.in" />
         <link rel="author" href="https://softmania.in" />
         <link rel="me" href="https://www.linkedin.com/company/softmania-tech/" />
